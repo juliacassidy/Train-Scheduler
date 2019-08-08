b1d1a190cda429e2c16daedf250de08d9bb6497a
Julia Cassidy <jac1007@wildcats.unh.edu>
2019-08-08 18:01:13 -0400

    added firebase and started java

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDSO7sevmaBx431uKhDtj7gPB-9rzKF8KM",
+    authDomain: "train-scheduler-afe7c.firebaseapp.com",
+    databaseURL: "https://train-scheduler-afe7c.firebaseio.com",
+    projectId: "train-scheduler-afe7c",
+    storageBucket: "",
+    messagingSenderId: "423852218050",
+    appId: "1:423852218050:web:ebb5838cb2bb129f"
+  };
 
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+var trainData = firebase.database();
+
+$("#addTrainBtn").on("click",function(){
+    var trainName = $("#trainNameInput").val().trim();
+    var destination = $("#destinationInput").val().trim();
+    var firstTrain = $("#firstTrainInput").val().trim();
+    var frequency = $("#frequencyInput").val().trim();
+})
